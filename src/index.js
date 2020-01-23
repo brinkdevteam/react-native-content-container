e66d0018803ffd1aff2da316c0b77adf814c2c92
aaronlbrink <aaron@brinkdatabase.com>
2020-01-23 16:32:45 -0500

    Fix

--- src/index.js
+++ src/index.js
@@ -3,32 +3,5 @@ import { View, Platform, StyleSheet } from "react-native";
 import { ContentSafeArea } from "./SafeAreaContent";
 
 export const Content = ({ children, style }) => {
-  <ContentSafeArea style={[localStyles.mainContainer, style]}>{children}</ContentSafeArea>;
+  <ContentSafeArea>{children}</ContentSafeArea>;
 };
-
-function getStatusBarHeight(safe) {
-  return Platform.select({
-    ios: ifIphoneX(safe ? 44 : 30, 20),
-    android: StatusBar.currentHeight,
-    default: 0
-  });
-}
-
-let topPadding;
-if (Platform.OS === "ios") {
-  topPadding = getStatusBarHeight() + 20;
-} else {
-  topPadding = 22;
-}
-const localStyles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1
-  },
-  mainContainer: {
-    paddingHorizontal: 32,
-    paddingTop: topPadding,
-    flex: 1,
-    alignItems: "center",
-    borderWidth: 0
-  }
-});
